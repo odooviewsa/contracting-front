30bfafb7d73007d2a915bbd159286d104000900b
AhmedRashad654 <ahmedrashad6542@gmail.com>
2024-12-01 03:59:47 +0200

    ahmed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://shark-app-wlj67.ondigitalocean.app/",
+  build: {
+    outDir: "build",
+  },
+  base: "/",
 });
