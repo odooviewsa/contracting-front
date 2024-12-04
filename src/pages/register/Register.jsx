dc437fb6c06b8f3afc84b8196151be53f58ef421
drelwyrady <48725038+drelwyrady@users.noreply.github.com>
2024-12-04 09:23:44 +0200

    Update Register - Elwy

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,107 +13,83 @@ export default function Register() {
     watch,
     control,
     register,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
   const password = watch("password");
+
   function onSubmit(data) {
-    sessionStorage.setItem("data_register",JSON.stringify(data));
+    sessionStorage.setItem("data_register", JSON.stringify(data));
     navigate("/register_complete");
   }
+
   return (
-    <div className="w-full min-h-screen bgLogin flex items-center justify-between p-5 overflow-x-hidden text-white">
-      <div className="lg:w-[49%] w-[100%] flex justify-center items-center">
-        <div className="w-[90%] flex flex-col gap-5 items-center">
+    <div className="w-full min-h-screen flex items-center justify-center lg:justify-between p-5 overflow-x-hidden text-white bg-[#06385c]">
+      <div className="lg:w-1/2 w-full flex justify-center items-center">
+        <div className="w-full max-w-md flex flex-col gap-5 items-center">
           <SignWithGoogle title="Create Account" />
-          <form
-            className="flex flex-col gap-4 w-full"
-            onSubmit={handleSubmit(onSubmit)}
-          >
+
+          <form className="flex flex-col gap-5 w-full" onSubmit={handleSubmit(onSubmit)}>
             {[
-              { label: "Company Email", type: "email", name: "email" },
-              { label: "Password", type: "password", name: "password" },
-              {
-                label: "Confirm Password",
-                type: "password",
-                name: "confirmPassword",
-              },
-              { label: "Phone number", type: "number", name: "phone" },
-            ].map((email, item) => {
-              if (email.name === "phone") {
+              { label: "Company Email", type: "email", name: "email", validation: { pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ } },
+              { label: "Password", type: "password", name: "password", validation: { minLength: { value: 6, message: "Password must be at least 6 characters long" } } },
+              { label: "Confirm Password", type: "password", name: "confirmPassword", validation: { validate: value => value === password || "Passwords do not match" } },
+              { label: "Phone Number", type: "number", name: "phone" },
+            ].map((field, index) => {
+              if (field.name === "phone") {
                 return (
-                  <div className="flex flex-col gap-2 w-full" key={item}>
-                    <label className="text-white">
-                      Phone Number <span className="text-red-400 ">*</span>
+                  <div key={index} className="flex flex-col gap-2 w-full">
+                    <label className="text-gray-100 font-medium">
+                      {field.label} <span className="text-red-400">*</span>
                     </label>
                     <Controller
-                      name={email.name}
+                      name={field.name}
                       control={control}
-                      rules={{ required: `${email.label} is required` }}
+                      rules={{ required: `${field.label} is required` }}
                       render={({ field }) => (
-                        <PhoneInput {...field} country={"eg"} />
+                        <PhoneInput {...field} country={"eg"} containerClass="text-black" />
                       )}
                     />
-                    <p className="text-red-400 text-[0.8rem] -mb-3">
-                      {errors[email.name] && errors[email.name].message}
-                    </p>
+                    {errors[field.name] && <p className="text-red-500 text-sm">{errors[field.name].message}</p>}
                   </div>
                 );
               } else {
                 return (
-                  <div className="flex flex-col gap-2 w-full" key={item}>
-                    <label htmlFor="">
-                      {email.label} <span className="text-red-400">*</span>
+                  <div key={index} className="flex flex-col gap-2 w-full">
+                    <label className="text-gray-100 font-medium">
+                      {field.label} <span className="text-red-400">*</span>
                     </label>
                     <input
-                      type={email.type}
-                      placeholder={email.label}
-                      className="py-2 px-3 placeholder:text-gray-400 outline-none border border-white rounded-md bg-transparent"
-                      {...register(email.name, {
-                        required: `${email.name} is required`,
-                        minLength:
-                          email.name === "password"
-                            ? {
-                                value: 6,
-                                message: "min length 6 character",
-                              }
-                            : undefined,
-                        validate:
-                          email.name === "ConfirmPassword"
-                            ? (value) =>
-                                value === password ||
-                                "   Confirm Password not match"
-                            : undefined,
-                        pattern:
-                          email.name === "email"
-                            ? {
-                                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                                message: "invalid email",
-                              }
-                            : undefined,
-                      })}
+                      type={field.type}
+                      placeholder={`Enter ${field.label}`}
+                      className={`py-2 px-3 placeholder-gray-400 outline-none border ${errors[field.name] ? "border-red-500" : "border-gray-300"} rounded-md bg-white text-black`}
+                      {...register(field.name, { required: `${field.label} is required`, ...field.validation })}
                     />
-                    <p className="text-red-400 text-[0.8rem] -mb-3">
-                      {errors[email.name] && errors[email.name].message}
-                    </p>
+                    {errors[field.name] && <p className="text-red-500 text-sm">{errors[field.name].message}</p>}
                   </div>
                 );
               }
             })}
-            <div className="flex items-center justify-between  font-semibold ">
-              <Link to={"/"} className="text-black text-[0.9rem]">
-                Already have account?{" "}
-                <span className=" cursor-pointer text-white"> Login</span>
+
+            <div className="flex items-center justify-between mt-4 font-semibold text-sm text-gray-300">
+              <Link to="/" className="hover:underline">
+                Already have an account? <span className="text-blue-400">Login</span>
               </Link>
-              <button className="flex items-center gap-2 text-white ">
-                <span>Next</span>
-                <FaArrowRightLong />
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={`flex items-center gap-2 text-lg font-semibold rounded-lg py-2 px-4 bg-blue-600 hover:bg-blue-700 transition duration-300 ${isSubmitting ? "opacity-50 cursor-not-allowed" : ""}`}
+              >
+                {isSubmitting ? "Processing..." : "Next"}
+                {!isSubmitting && <FaArrowRightLong />}
               </button>
             </div>
           </form>
         </div>
       </div>
-      <div className="lg:w-[49%] hidden lg:flex justify-center items-center containerImage relative">
-        <img src={imageLogin} alt="login" className="w-[90%]" />
+
+      {/* Image Section (only on large screens) */}
+      <div className="lg:w-1/2 hidden lg:flex justify-center items-center">
+        <img src={imageLogin} alt="Register" className="w-3/4 max-w-md" />
       </div>
     </div>
   );
